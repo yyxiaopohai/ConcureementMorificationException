3835e8323efcbf30ab0ba1ccc192d809088845bd
小张 <yyxiaozhang@foxmail.com>
2019-12-16 18:33:17 +0800

    集合在多线程中出现--并发修改异常--解决方案

--- Thread/src/com/atguigu/thread/ListDemo.java
+++ Thread/src/com/atguigu/thread/ListDemo.java
@@ -1,15 +1,55 @@
 package com.atguigu.thread;
 
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ListDemo {
     public static void main(String[] args) {
 
+        //---------------------------map---------------------------
+        Map<String,String> map = new ConcurrentHashMap<>();//new HashMap<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(()->{
+                map.put(Thread.currentThread().getName(),UUID.randomUUID().toString().substring(0,6));
+                System.out.println(map);
+            },String.valueOf(i)).start();
+        }
 
+
+        //---------------------------set---------------------------
+        /*Set<String> set = new CopyOnWriteArraySet<>();//Collections.synchronizedSet(new HashSet<>());//new HashSet();
+        for (int i = 0; i < 30; i++) {
+            new Thread(()->{
+                set.add(UUID.randomUUID().toString().substring(0,6));
+                System.out.println(set);
+            },String.valueOf(i)).start();
+        }*/
+
+        //---------------------------list---------------------------
         /*List list =new ArrayList(); //new CopyOnWriteArrayList() ;//new Vector();//Collections.synchronizedList(new ArrayList<>());
         for (int i = 0; i < 30; i++) {
             new Thread(()->{
                 list.add(UUID.randomUUID().toString().substring(0,6));
                 System.out.println(list);
             },String.valueOf(i)).start();
+        }*/
+
+        /*
+            ------源码------
+        public boolean add(E e) {
+            final ReentrantLock lock = this.lock;
+            lock.lock();
+            try {
+                Object[] elements = getArray();
+                int len = elements.length;
+                Object[] newElements = Arrays.copyOf(elements, len + 1);
+                newElements[len] = e;
+                setArray(newElements);
+                return true;
+            } finally {
+                lock.unlock();
+            }
         }*/
     }
 }
