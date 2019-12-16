9c01f881748c575dd583e842ba343195fec99747
小张 <yyxiaozhang@foxmail.com>
2019-12-16 21:07:59 +0800

    抢车位--有来有往

--- Thread/src/com/atguigu/thread/CyclicBarrierTest.java
+++ Thread/src/com/atguigu/thread/CyclicBarrierTest.java
@@ -8,6 +8,7 @@ public class CyclicBarrierTest {
     public static void main(String[] args) {
         CyclicBarrier cyclicBarrier = new CyclicBarrier(7,()->{ System.out.println("------召唤神龙------"); });
 
+
         for (int i = 1; i <= 7; i++) {
             final int temp = i;
             new Thread(()->{
