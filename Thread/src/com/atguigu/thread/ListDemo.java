9a07b7c1c399a9e80385d298a77dc2d540b14b44
小张 <yyxiaozhang@foxmail.com>
2019-12-16 11:11:21 +0800

    JUC

---
+++ Thread/src/com/atguigu/thread/ListDemo.java
@@ -0,0 +1,15 @@
+package com.atguigu.thread;
+
+public class ListDemo {
+    public static void main(String[] args) {
+
+
+        /*List list =new ArrayList(); //new CopyOnWriteArrayList() ;//new Vector();//Collections.synchronizedList(new ArrayList<>());
+        for (int i = 0; i < 30; i++) {
+            new Thread(()->{
+                list.add(UUID.randomUUID().toString().substring(0,6));
+                System.out.println(list);
+            },String.valueOf(i)).start();
+        }*/
+    }
+}
